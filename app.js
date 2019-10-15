679d000e494bcd7ed92b358bc080211eef113430
joshlopezz <lopezzamudio_joshua@columbusstate.edu>
2019-10-15 23:06:13 +0000

    added add task

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 var http = require('http');
 var path = require('path');
 const express = require("express");
+var bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({encoded: true}));
+
+var task = ["do hw", "workout"];
 
 app.get('/', function(req, res){
     res.render("index");
 });
 
-app.get('/about', function(){
-    res.send("about page");
+app.post('/addtask', function(req, res){
+    var newTask = req.body.newtask;
+    task.push(newTask);
+    res.redirect("/");
 });
 
 http.createServer(app).listen(port, function(){
