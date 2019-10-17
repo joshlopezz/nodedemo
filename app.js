75e1c6d872b5455e3b10d635e7f9169f128264e7
joshlopezz <lopezzamudio_joshua@columbusstate.edu>
2019-10-17 22:50:57 +0000

    made changes

--- app.js
+++ app.js
@@ -10,15 +10,30 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({encoded: true}));
 
 var task = ["do hw", "workout"];
+var complete = ["finish lab", "do laundry"];
 
 app.get('/', function(req, res){
-    res.render("index", {task:task});
+    res.render("index", {task:task, complete:complete});
 });
 
 app.post('/addtask', function(req, res){
     var newTask = req.body.newtask;
     task.push(newTask);
     res.redirect("/");
+});
+
+app.post('/removetask', function(req, res){
+    var completeTask = req.body.check;
+    if(typeof completeTask === "string"){
+        complete.push(completeTask);
+        task.splice(task.indexOf(completeTask), 1);
+    }else if (typeof completeTask === "object"){
+        for(var i = 0; i < completeTask.length ; i++){
+            complete.push(completeTask[i]);
+            task.splice(task.indexOf(completeTask[i]), 1);
+        }
+    }
+    res.redirect('/');
 });
 
 http.createServer(app).listen(port, function(){
