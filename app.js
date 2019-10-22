c89a8115da8bf1b5bccd72efb3e294f586047cfd
joshlopezz <lopezzamudio_joshua@columbusstate.edu>
2019-10-22 22:55:03 +0000

    added mongoose and mongodb functionality

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 var http = require('http');
 var path = require('path');
 var express = require("express");
+var mongoose = require("mongoose");
 var bodyParser = require('body-parser');
 var app = express();
 var port = process.env.PORT || 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.static("css"));
-app.use(bodyParser.urlencoded({encoded: true}));
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({encoded: false}));
+const Todo = require('./models/todo_model');
+const mongoDB = 'mongodb+srv://char:1234@cluster0-m93iw.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, "MongoDB connection error:"));
+
 
 var task = ["do hw", "workout"];
 var complete = ["finish lab", "do laundry"];
@@ -18,9 +27,16 @@ app.get('/', function(req, res){
 });
 
 app.post('/addtask', function(req, res){
-    var newTask = req.body.newtask;
-    task.push(newTask);
-    res.redirect("/");
+    let newTodo = new Todo({
+        item: req.body.newtask,
+        done: false
+    });
+    newTodo.save(function(err){
+        if(err){
+            console.log(err);
+        }
+        res.redirect('/');
+    });
 });
 
 app.post('/removetask', function(req, res){
